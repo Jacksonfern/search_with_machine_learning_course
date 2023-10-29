b86d4aea0ad1086213546be50e2db59cf0af67fe
Jackson Fernandes <jackson.fernandes@icomp.ufam.edu.br>
2023-10-29 19:31:28 +0000

    fea using m broader categories for classifying products

--- week2/createContentTrainingData.py
+++ week2/createContentTrainingData.py
@@ -5,13 +5,21 @@ from tqdm import tqdm
 import os
 import xml.etree.ElementTree as ET
 from pathlib import Path
+import re
 
 def transform_name(product_name):
-    # IMPLEMENT
-    return product_name
+    # strip space characters at the beginning and at the end of the text 
+    transformed_name = product_name.strip().lower()
+    # remove all non non-alphanumeric characters other than underscore
+    transformed_name = re.sub(r'[^A-Za-z0-9_\-\s]', '', transformed_name)
+    # trim excess space characters 
+    transformed_name = re.sub(r'\s+', ' ', transformed_name)
+
+    return transformed_name
 
 # Directory for product data
 directory = r'/workspace/datasets/product_data/products/'
+categories_filename = r'/workspace/datasets/product_data/categories/categories_0001_abcat0010000_to_pcmcat99300050000.xml'
 
 parser = argparse.ArgumentParser(description='Process some integers.')
 general = parser.add_argument_group("general")
@@ -57,20 +65,42 @@ def _label_filename(filename):
               labels.append((cat, transform_name(name)))
     return labels
 
-def filter_min_products(all_labels, min_freq):
+def get_ancestors_category(categories_filename, label):
+    tree = ET.parse(categories_filename)
+    root = tree.getroot()
+    cats_anc = {}
+
+    for child in root:
+        cur_cat = child.find(label).text
+        cats_anc[cur_cat] = cur_cat
+        deep = 0
+
+        cat_path = child.find('path')
+        for cat in cat_path:
+            cat_id = cat.find(label).text
+            deep += 1
+            if deep == 2:
+                cats_anc[cur_cat] = cat_id
+                break
+    return cats_anc
+
+def filter_min_products(all_labels, min_freq, categories_filename, label):
+    cat_ancs = get_ancestors_category(categories_filename, label)
     cats_count = {}
     for label_list in all_labels:
         for (cat, _) in label_list:
-            if cat in cats_count:
-                cats_count[cat] += 1
+            anc_cat = cat_ancs.get(cat, cat)
+            if anc_cat in cats_count:
+                cats_count[anc_cat] += 1
             else:
-                cats_count[cat] = 1
+                cats_count[anc_cat] = 1
     
     filtered_labels = []
     for label_list in all_labels:
-        for label_item in label_list:
-            if cats_count[label_item[0]] > min_freq:
-                filtered_labels.append(label_item)
+        for (cat, name) in label_list:
+            anc_cat = cat_ancs.get(cat, cat)
+            if cats_count[anc_cat] >= min_freq:
+                filtered_labels.append((anc_cat, name))
 
     return filtered_labels
 
@@ -79,7 +109,7 @@ if __name__ == '__main__':
     print("Writing results to %s" % output_file)
     with multiprocessing.Pool() as p:
         all_labels = tqdm(p.imap(_label_filename, files), total=len(files))
-        filtered_labels = filter_min_products(list(all_labels), min_products)
+        filtered_labels = filter_min_products(list(all_labels), min_products, categories_filename, args.label)
         with open(output_file, 'w') as output:
             for (cat, name) in filtered_labels:
                 output.write(f'__label__{cat} {name}\n')
